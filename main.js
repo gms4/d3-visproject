a46e827f34e4d9388f2fd8f52f3c49b48b201b52
gms4 <gms4@cin.ufpe.br>
2022-04-14 18:29:08 -0300

    fixing circles and databinding

--- main.js
+++ main.js
@@ -1,71 +1,173 @@
 const fetchText = async (url) => {
+
   const response = await fetch(url);
   return await response.text();
+
 };
 
-let corPartidoMap = new Map();
-const getCorPartidor = (partido) => {
-  if (corPartidoMap.has(partido)) {
-    return corPartidoMap.get(partido);
-  } else {
-    let randomColor = [];
-    randomColor.push(Math.floor(Math.random() * 120) + 100);
-    randomColor.push(Math.floor(Math.random() * 120) + 100);
-    randomColor.push(Math.floor(Math.random() * 120) + 100);
-    return corPartidoMap.set(
-      partido,
-      "rgb(" +
-        randomColor[0] +
-        ", " +
-        randomColor[1] +
-        ", " +
-        randomColor[2] +
-        ")"
-    );
-  }
-};
+//pegando as cores dos partidos
+//fonte https://pt.wikipedia.org/wiki/Predefinição:Cor_de_partido_pol%C3%ADtico/BRA
+function getPartyColor (party) {
 
-const csvUrl = "./Senadores.csv";
-// //"https://raw.githubusercontent.com/nivan/testPython/main/ListaParlamentarEmExercicio.csv";
+    switch (party) {
 
-let data;
+      case "MDB": 
+      
+        return "#00aa4f"
 
-fetchText(csvUrl)
+      case "PL":
+
+        return "#FF4500"
+
+      case "PSDB":
+
+        return "#0080FF"
+
+      case "PSD":
+
+        return "#FFA500"
+
+      case "PDT":
+
+        return "#FE8E6D"
+
+      case "PROS":
+
+        return "#f48c24"
+
+      case "PT":
+
+        return "#c4122d"
+
+      case "CIDADANIA":
+
+        return "#ec008c"
+
+      case "DEM":
+
+        return "#97e500ff"
+
+      case "PODEMOS":
+
+        return "#31b44b"
+
+      case "PP":
+
+        return "#0067A5"
+
+      case "REPUBLICANOS":
+
+        return "#115E80"
+
+      case "PSL":
+
+        return "#054577"
+
+      case "REDE":
+
+        return "#00C2BB"
+
+        default: return "#000000"
+    }
+
+
+}
+
+//usando interpolação pra plotar os círculos
+
+const width = 850;
+const height = 650;
+
+const range = (index) => {
+  if(index < 10) return [0, 10]
+  if(index < 23) return [10, 23]
+  if(index < 42) return [23, 42]
+  if(index < 60) return [42, 60]
+  if(index < 81) return [60, 81]
+}
+const getRadius = (index) => {
+  if(index < 10) return 150
+  if(index < 23) return 190
+  if(index < 42) return 230
+  if(index < 60) return 270
+  if(index < 81) return 310
+}
+
+//offset = -5 é giradinha gambiarra pra não ficar tão feio assim
+//não consegui estruturar direitinho pra ficar retinho
+const offset = -5
+const mapRange = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2;
+function interpolate(p0, p1, t){
+  return (1-t)*p0 + t*p1;
+}
+const getX = (index) => {
+  const angle = mapRange(index, ...range(index), 270, 90)
+  return Math.sin((angle+offset) * Math.PI/180) * getRadius(index) + (width/2)
+}
+const getY = (index) => {
+  const angle = mapRange(index, ...range(index), 270, 90)
+  return Math.cos((angle+offset) * Math.PI/180) * getRadius(index) + (height)
+}
+
+//d3 aqui
+//vamos pegar os dados do csv fornecido e manipulá-los
+let csvData;
+
+const csv = "./Senadores.csv";
+
+fetchText(csv)
   .then((text) => {
+
     //tratar dados
-    data = d3.csvParse(text);
-    return data;
+    csvData = d3.csvParse(text);
+    return csvData;
+
   })
-  .then((data) => {
-    //ações
-    let dataIndex = 8;
-    let currSenador = data[dataIndex];
+  .then((csvData) => {
 
-    let fullSvg = d3.select("div").select("svg");
-    let group = fullSvg.select("g");
+    //setando imagem do senador, nome do senador e partido e conectando com o html em img, h1 e h2 respectivamente
+    let imageSenator = d3.select("img");
+    let labelNameSenator = d3.select("h1");
+    let labelParty = d3.select("h2");
 
-    let image = d3.select("img");
-    let labelNome = d3.select("h1");
-    let labelPartido = d3.select("h2");
-
-    let circles = group.selectAll("circle");
+    //usando a função do d3 select all como visto em aula p fazer o data binding
+    let circles = d3.select("div").select("svg").selectAll("circle");
     circles
-      .data(data)
-      .style("fill", (d) => {
-        return getCorPartidor(d.SiglaPartidoParlamentar);
+      .data(csvData)
+      .enter()  
+      .append('circle')
+      .attr('r', 10)
+      .attr('cx', d=>getX(d.id))
+      .attr('cy', d=>getY(d.id))
+      .attr('fill', d=>getPartyColor(d.siglaPart))
+      .text((d) => d.nomeSenador + " " + d.siglaPart)
+      .on("click", (event, d) => {
+        imageSenator.attr("src", () => {
+          return d.urlFoto;
+        })
+        
+        //setando a cor do nome do partido pra ser igual a cor do partido
+        //e o nome dos senadores setei em branco no html
+        labelNameSenator.text(d.nomeSenador);
+        labelParty
+          .style("color", getPartyColor(d.siglaPart))
+          .text(d.siglaPart);
       })
-      .text((d) => d.NomeParlamentar + " " + d.SiglaPartidoParlamentar)
-      .on("click", function (event, d) {
-        image.attr("src", () => {
-          return d.UrlFotoParlamentar;
-        });
 
-        labelNome.text(d.NomeParlamentar);
+      //setando o reset das bordas quando o mouse sai de cima
+      .on("mouseleave", (event, d) => {
+        d3.select(event.target)
+        .transition()
+        .duration(100)
+        .style('stroke-width', '0px');
+      })
 
-        labelPartido
-          .style("color", getCorPartidor(d.SiglaPartidoParlamentar))
-          .text(d.SiglaPartidoParlamentar);
-      });
+      //setando as bordas quando passa o mouse por cima
+      .on("mouseover", (event, d) => {
+        d3.select(event.target)
+        .transition()
+        .duration(100)
+        .style('stroke', 'white')
+        .style('stroke-width', '2px');
+      })
   });
-
-//svgTitle.innerHTML = nome;
